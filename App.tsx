cebdd770cee404a015edc6922974a2fb2577568c
Saye <mohammadjavadnoctis2@gmail.com>
2023-01-18 08:10:29 +0300

    Buttons and open gllery functionality

--- App.tsx
+++ App.tsx
@@ -1,19 +1,43 @@
 import { Entypo } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { useEffect, useState } from 'react';
 
 
 const PlaceholderImage = require('./assets/images/someImage.jpg');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null)
+  useEffect(() => {
+      console.log('this is the selected image', selectedImage)
+  },[selectedImage])
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1
+    })
+    console.log('hi')
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  } 
+ const img = selectedImage !== null
+ ? { uri: selectedImage }
+ : PlaceholderImage;
   return (
     <View className="flex-1 items-center justify-center">
       <StatusBar style="auto" />
       <View >
-        <Image source={PlaceholderImage} className="w-[320px] h-[440px] rounded-xl"/>
+        {/* @ts-ignore */}
+        <Image source={img} className="w-[320px] h-[440px] rounded-xl"/>
       </View>
       <View className=' mt-4'>
-          <Pressable className='border border-orange-400 mt-4 flex flex-row items-center justify-around px-2' onPress={() => {alert("you pressed the button")}}>
+          <Pressable className='border border-orange-400 mt-4 flex flex-row items-center justify-around px-2' onPress={pickImage}>
                 <Entypo name='folder-images' color={'orange'} size={20} />
               <Text className='px-8 py-4'> 
                 Choose an image
---
+++ components/ImageViewver/ImageViewer.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react'
+
+interface ImageViewerProps {
+    
+}
+
+
+const ImageViewer: FC = () => {
+  return (
+    <div>ImageViewer</div>
+  )
+}
+
+export default ImageViewer
